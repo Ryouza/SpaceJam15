38a33b1645bc96a0413dd362e6ca949e2a45340e
Nicholas Dedenbach <ndeden@umich.edu>
2015-01-16 21:11:22 -0600

    Added rubber material, continued block dev

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class Block : MonoBehaviour {
 
+	public bool			    bouncy;
+	public int				health;
+	public int				solid;
+
 	// Use this for initialization
 	void Start () {
-	
+		if (bouncy) {
+			PhysicMaterial mat = Resources.Load ("Rubber", typeof(PhysicMaterial)) as PhysicMaterial;
+			this.collider.material = mat;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (bouncy) {
+			PhysicMaterial mat = Resources.Load ("Rubber", typeof(PhysicMaterial)) as PhysicMaterial;
+			this.collider.material = mat;
+		}
 	}
+
 }
