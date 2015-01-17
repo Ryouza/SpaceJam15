74ff1f1aa269a3c61c53e18d118fc818d1f36123
Nicholas Dedenbach <ndeden@umich.edu>
2015-01-16 23:29:36 -0600

    More block stuff, added resources folder

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -5,22 +5,39 @@ public class Block : MonoBehaviour {
 
 	public bool			    bouncy;
 	public int				health;
-	public int				solid;
+	public bool				solid;
+	public bool				indestructible;
 
 	// Use this for initialization
 	void Start () {
+		this.collider2D.enabled = false;
 		if (bouncy) {
-			PhysicMaterial mat = Resources.Load ("Rubber", typeof(PhysicMaterial)) as PhysicMaterial;
-			this.collider.material = mat;
+			PhysicsMaterial2D mat = Resources.Load ("Physics Materials/Rubber", typeof(PhysicsMaterial2D)) as PhysicsMaterial2D;
+			if (mat == null) {
+				Debug.Log ("Material not loaded.");
+			}
+			this.collider2D.sharedMaterial = mat;
+		}
+		if (!solid) {
+			this.collider2D.isTrigger = true;
+		} else {
+			this.collider2D.enabled = true;
+			this.collider2D.isTrigger = false;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (bouncy) {
-			PhysicMaterial mat = Resources.Load ("Rubber", typeof(PhysicMaterial)) as PhysicMaterial;
-			this.collider.material = mat;
-		}
+
 	}
 
+	void OnCollisionEnter2D(Collision2D coll) {
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.tag == "Weapon" && !indestructible) {
+			this.health -= 1;// coll.gameObject.GetComponent<Attack>.damage;
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+		}
+	}
 }
