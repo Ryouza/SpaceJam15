bbcbcb810da9be2fa3dfb6abb6bd569424632778
Alex <alchkim@umich.edu>
2015-01-17 14:03:41 -0500

    Attack cs and Health cs

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+	float jack_damage = 0;
+	float monster_damage = 0;
+
+	void Damage(GameObject a, ref GameObject b) {
+		Health health = b.GetComponent< Health > ();
+		if (a.gameObject.tag == "Weapon") {
+			health.health -= jack_damage;
+		}
+		if (a.gameObject.tag == "Monster") {
+			health.health -= monster_damage;
+		}
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	public float health = 1;
+		
+	public Health (string tag) {
+		if (tag == "block") {
+			health = 1;
+		}
+		if (tag == "Player") {
+			health = 5;
+		}
+		if (tag == "Monster") {
+			health = 2;
+		}
+	}
+
+	void Start () {
+
+	}
+
+	void Update () {
+		if (health <= 0) {
+			Destroy (this.gameObject);
+		}
+	}
+}
