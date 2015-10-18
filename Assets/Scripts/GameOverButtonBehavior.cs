85c6e640f5e40453fb38bfa959ad5f5d418f6e2a
joshviss <joshviss@umich.edu>
2015-10-18 00:07:59 -0400

    Changes to bookshelf and door.

---
+++ Assets/Scripts/GameOverButtonBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverButtonBehavior : MonoBehaviour {
+
+	public void RestartLevel() {
+		//reloads the current level.
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void RestartGame() {
+		//Loads the MainMenu.
+		Application.LoadLevel ("MainMenu");
+	}
+
+
+
+}
